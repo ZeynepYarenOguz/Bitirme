c960163d6649d6ec28022e3164cfe85584042c00
irem inanç <inanc.irem98@gmail.com>
2020-11-28 21:01:31 +0300

    Add project files.

---
+++ bitirme/Chocolate_Desserts.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bitirme
+{
+    /// <summary>
+    /// Interaction logic for Chocolate_Daesserts.xaml
+    /// </summary>
+    public partial class Chocolate_Daesserts : Window
+    {
+        public Chocolate_Daesserts()
+        {
+            InitializeComponent();
+        }
+    }
+}
